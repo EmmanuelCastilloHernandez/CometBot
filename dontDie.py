1d86961914dec6451f33dc887b2c77a7e9a8db32
Emmanuel Castillo <>
2021-05-15 04:26:54 +0000

    Beta 4 of Luna

--- dontDie.py
+++ dontDie.py
@@ -2,16 +2,29 @@
 # INFERFERENCE
 from flask import Flask, render_template
 from threading import Thread
+import os
+
+websiteImgs = os.path.join('static', 'photoToRender')
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = websiteImgs
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+  imageToShow = os.path.join(app.config['UPLOAD_FOLDER'], 'CometLogo.jpg')
+  return render_template('index.html', user_image=imageToShow)
 
 @app.route('/features')
 def featuresPage():
   return render_template('features.html')
+
+@app.route('/versions')
+def versionsPage():
+  return render_template('versions.html')
+
+@app.route('/creators')
+def creatorsPage():
+  return render_template('creators.html')
 
 def run():
   app.run(host='0.0.0.0',port=8080)
