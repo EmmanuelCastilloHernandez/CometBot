51df312d8f57fc986e919d9c518ad59d8b8bb9d5
Emmanuel Castillo <>
2021-05-15 04:56:31 +0000

    Beta 5

--- dontDie.py
+++ dontDie.py
@@ -11,20 +11,23 @@ app.config['UPLOAD_FOLDER'] = websiteImgs
 
 @app.route('/')
 def home():
-  imageToShow = os.path.join(app.config['UPLOAD_FOLDER'], 'CometLogo.jpg')
+  imageToShow = os.path.join(app.config['UPLOAD_FOLDER'], 'favicon.png')
   return render_template('index.html', user_image=imageToShow)
 
 @app.route('/features')
 def featuresPage():
-  return render_template('features.html')
+  imageToShow = os.path.join(app.config['UPLOAD_FOLDER'], 'favicon.png')
+  return render_template('features.html', user_image=imageToShow)
 
 @app.route('/versions')
 def versionsPage():
-  return render_template('versions.html')
+  imageToShow = os.path.join(app.config['UPLOAD_FOLDER'], 'favicon.png')
+  return render_template('versions.html', user_image=imageToShow)
 
 @app.route('/creators')
 def creatorsPage():
-  return render_template('creators.html')
+  imageToShow = os.path.join(app.config['UPLOAD_FOLDER'], 'favicon.png')
+  return render_template('creators.html', user_image=imageToShow)
 
 def run():
   app.run(host='0.0.0.0',port=8080)
